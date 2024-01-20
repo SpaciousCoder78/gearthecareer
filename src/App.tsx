bff3653b0815294e1dc9d04af37e20b4805f34a0
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-01-21 00:58:11 +0530

    added more files

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@
 import './App.css'
 //import Index from './homepage'
 //import Astroflowchart from './flowcharts/Astronomer-flowchart'
-//import Busianalyst from './flowcharts/BusinessAnalyst-flowchart'
-import Psychchart from './flowcharts/Psychologist-flowchart'
+import Busianalyst from './flowcharts/BusinessAnalyst-flowchart'
+import Factsheet from './factsheet'
+//import Psychchart from './flowcharts/Psychologist-flowchart'
 //import Careerpath from './careerpath' // Import the Index component
 
 function App() {
   return (
     <>
-      <Psychchart/>
+      <Busianalyst/>
     </>
   )
 }
