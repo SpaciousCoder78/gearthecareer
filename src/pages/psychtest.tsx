89427731621534a3b489a4edb4d421befa376773
Susheel <susheelnani116@gmail.com>
2024-01-21 15:59:52 +0530

    psych test updated

--- src/pages/psychtest.tsx
+++ src/pages/psychtest.tsx
@@ -2,9 +2,9 @@ function psychtest(){
     return(
         <div>
             <h1 style={{ fontWeight: 'bold' }}>PSYCHOMETRIC TEST</h1>
-            <a href="https://mettl.com/psychometric-tests/?ads_cmpid=9569007296&ads_adid=105309234224&ads_targetid=kwd-204721576&ads_loc_intrst=&ads_loc_physcl=9299312&ads_network=g&ads_creative=650312523553&ads_kw_term=psychometric%20test&ads_adposition=&utm_source=adwords&utm_medium=ppc&utm_campaign=9569007296&gclid=Cj0KCQiAnrOtBhDIARIsAFsSe53aRczP0O4z49XrfLkfuRbY9Q3eD_z3YHCulyYpIzgGDidxlnlY1NgaAr_dEALw_wcB&gad_source=1" target="_blank">TEST-1</a>
-            <a href="https://www.psychometricinstitute.com.au/free_psychometric_test.html" target="_blank">TEST-2</a>
-            <a href="https://university-direct.com/careers-advice-support/psychometric-tests-and-psychometric-test-examples/" target="_blank">TEST-3</a>
+            <a href="https://mettl.com/psychometric-tests/?ads_cmpid=9569007296&ads_adid=105309234224&ads_targetid=kwd-204721576&ads_loc_intrst=&ads_loc_physcl=9299312&ads_network=g&ads_creative=650312523553&ads_kw_term=psychometric%20test&ads_adposition=&utm_source=adwords&utm_medium=ppc&utm_campaign=9569007296&gclid=Cj0KCQiAnrOtBhDIARIsAFsSe53aRczP0O4z49XrfLkfuRbY9Q3eD_z3YHCulyYpIzgGDidxlnlY1NgaAr_dEALw_wcB&gad_source=1" target="_blank" style={{ fontWeight: 'bold', fontSize: 'large' }}>TEST-1</a>
+            <a href="https://www.psychometricinstitute.com.au/free_psychometric_test.html" target="_blank" style={{ fontWeight: 'bold', fontSize: 'large' }}>TEST-2</a>
+            <a href="https://university-direct.com/careers-advice-support/psychometric-tests-and-psychometric-test-examples/" target="_blank" style={{ fontWeight: 'bold', fontSize: 'large' }}>TEST-3</a>
        
         </div>
 
