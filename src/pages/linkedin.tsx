22fdfe4f0c6b71981acacf690153b66dc8b46b36
Susheel <susheelnani116@gmail.com>
2024-01-21 13:36:02 +0530

    linkedln page

--- src/pages/linkedin.tsx
+++ src/pages/linkedin.tsx
@@ -1,9 +1,16 @@
-function Linkedin(){
+function Resources(){
     return(
         <div>
-            <h1>sdsdsdsd</h1>
+            <h1>
+               RESOURCES
+            </h1>
+            <img src="https://media.discordapp.net/attachments/1198163202937200682/1198532627397869658/linkedin-featured.jpg?ex=65bf3f7e&is=65acca7e&hm=14edcff9dae53f78c390be7771efee5b3a9b02b5800ab79e5fbc457b35fb78ab&=&format=webp&width=990&height=424" />
+            <p>In the dynamic landscape of professional networking, LinkedIn stands as a titan. It serves as a curated platform where seasoned executives, ambitious professionals, and thought leaders across diverse industries converge. Akin to a digital symposium, it fosters meaningful connections, knowledge exchange, and career advancement opportunities. Through meticulously crafted profiles showcasing expertise and achievements, individuals establish their professional identities and engage in a vibrant network. Job seekers leverage its robust search functionalities to discover hidden gems, while employers access a talent pool brimming with qualified candidates. Continuous learning flourishes through insightful articles, engaging discussions, and targeted courses, empowering individuals to hone their skills and stay ahead of the curve. Whether you seek new horizons, valuable collaborations, or a platform to amplify your voice, LinkedIn stands as an invaluable resource, ready to connect you with the right people, opportunities, and knowledge to propel your professional journey.
+
+</p>
         </div>
+        
+    
     );
 }
-
-export default Linkedin
+export default Resources
