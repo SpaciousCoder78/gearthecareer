b904c0c59d4a6cb218482b334c6ce97f8fe68214
Susheel <susheelnani116@gmail.com>
2024-01-21 13:44:45 +0530

    fact sheets

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -2,10 +2,12 @@ function Resources(){
     return(
         <div>
             <h1>
-                heekekfmf
+               RESOURCES
             </h1>
+            <img src="https://media.discordapp.net/attachments/1198163202937200682/1198532627397869658/linkedin-featured.jpg?ex=65bf3f7e&is=65acca7e&hm=14edcff9dae53f78c390be7771efee5b3a9b02b5800ab79e5fbc457b35fb78ab&=&format=webp&width=990&height=424" />
         </div>
+        
+    
     );
 }
-
 export default Resources
