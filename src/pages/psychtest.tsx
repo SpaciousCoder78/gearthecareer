7d358e58120a2b2b1c6d89921a359426d88de327
Susheel <susheelnani116@gmail.com>
2024-01-21 15:51:10 +0530

    TEST LINKS

--- src/pages/psychtest.tsx
+++ src/pages/psychtest.tsx
@@ -1,8 +1,13 @@
 function psychtest(){
     return(
         <div>
-            <h1>ddfdsfdsfdsfdsfd</h1>
+            <h1 style={{ fontWeight: 'bold' }}>PSYCHOMETRIC TEST</h1>
+            <a href="https://mettl.com/psychometric-tests/?ads_cmpid=9569007296&ads_adid=105309234224&ads_targetid=kwd-204721576&ads_loc_intrst=&ads_loc_physcl=9299312&ads_network=g&ads_creative=650312523553&ads_kw_term=psychometric%20test&ads_adposition=&utm_source=adwords&utm_medium=ppc&utm_campaign=9569007296&gclid=Cj0KCQiAnrOtBhDIARIsAFsSe53aRczP0O4z49XrfLkfuRbY9Q3eD_z3YHCulyYpIzgGDidxlnlY1NgaAr_dEALw_wcB&gad_source=1" target="_blank">TEST-1</a>
+            <a href="https://www.psychometricinstitute.com.au/free_psychometric_test.html" target="_blank">TEST-2</a>
+            <a href="https://university-direct.com/careers-advice-support/psychometric-tests-and-psychometric-test-examples/" target="_blank">TEST-3</a>
+       
         </div>
+
     );
 }
 export default psychtest
