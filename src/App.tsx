ca0167ea68545dad8a3b021b741c36fbb1c00259
Susheel <susheelnani116@gmail.com>
2024-01-21 08:01:34 +0530

    aryan's branch enter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 
 import './App.css'
-//import Index from './homepage'
+import Index from './homepage'
 //import Astroflowchart from './flowcharts/Astronomer-flowchart'
 //import Busianalyst from './flowcharts/BusinessAnalyst-flowchart'
-import Factsheet from './factsheet'
+//import Factsheet from './factsheet'
 //import Psychchart from './flowcharts/Psychologist-flowchart'
 //import Careerpath from './careerpath' // Import the Index component
 
 function App() {
   return (
     <>
-      <Factsheet/>
+      <Index/>
     </>
   )
 }
